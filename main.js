08762244a70c0c7c64d7ad07be9b688d442f5140
vian21 <pigir101@uottawa.ca>
2023-09-27 12:30:32 -0400

    feat: export and import pages
    - add page to export data saved in localstorage as JSON
    - Import page expect JSON array which it then appends to data already in
      storage

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
+/**
+ * @returns array of weight data
+ */
 function getData() {
-    if (localStorage.getItem('weightData')) {
-        try {
-            return JSON.parse(localStorage.getItem('weightData'))
-        }
-        catch {
-            return JSON.parse('[]')
-        }
-
-    } else {
-        localStorage.setItem('weightData', '');
-        return JSON.parse('[]')
+  if (localStorage.getItem("weightData")) {
+    try {
+      return JSON.parse(localStorage.getItem("weightData"));
+    } catch {
+      return JSON.parse("[]");
     }
+  } else {
+    localStorage.setItem("weightData", "");
+    return JSON.parse("[]");
+  }
 }
 
 /**
- * 
+ *
  * Save array to localstorage
  * @param data array
  */
 function updateStorage(data) {
-    localStorage.setItem('weightData', JSON.stringify(data))
-    return true;
+  localStorage.setItem("weightData", JSON.stringify(data));
+  return true;
 }
